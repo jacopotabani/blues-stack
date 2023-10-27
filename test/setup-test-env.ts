431de9684694a3f5470f2e04e0337a88d4ec81d4
jacopotaba <jacopotabanimwm@gmail.com>
2023-10-27 10:51:35 +0200

    setup remix testing

--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -1,4 +1,11 @@
 import { installGlobals } from "@remix-run/node";
 import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
 
 installGlobals();
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
